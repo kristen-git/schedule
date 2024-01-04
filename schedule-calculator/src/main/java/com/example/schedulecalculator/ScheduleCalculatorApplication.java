39215a2b8f65652d2582a743f19115c00a0da47f
kristen-git <18795975110@163.com>
2024-01-04 09:16:17 +0800

    Initial commit

---
+++ schedule-calculator/src/main/java/com/example/schedulecalculator/ScheduleCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedulecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleCalculatorApplication.class, args);
+    }
+
+}
---
+++ schedule-calculator/src/test/java/com/example/schedulecalculator/ScheduleCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedulecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
